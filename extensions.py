a54348e5364abef1032ad6513b8921b9f7fbb58c
EmmanuelDanso1 <kwekudanso21@gmail.com>
2025-12-18 13:56:14 +0000

    new update

--- extensions.py
+++ extensions.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 from sqlalchemy import MetaData
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-
+from urllib.parse import quote_plus
 # Constraint naming convention for Alembic
 metadata = MetaData(
     naming_convention={
